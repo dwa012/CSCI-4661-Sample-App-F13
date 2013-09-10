006c58451d8bef4aebd6f5f33e6833672878e5ff
Daniel Ward <dwa012@gmail.com>
2013-09-10 17:39:36 -0500

    adding some fragments to the application, in preparation for the first lab

---
+++ GroceryList/src/main/java/edu/uno/csci4661/grocerylist/ui/DetailFragment.java
@@ -0,0 +1,17 @@
+package edu.uno.csci4661.grocerylist.ui;
+
+import android.app.Fragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+/**
+ * Created by danielward on 9/8/13.
+ */
+public class DetailFragment extends Fragment {
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container, Bundle savedInstanceState) {
+        return super.onCreateView(inflater, container, savedInstanceState);
+    }
+}
