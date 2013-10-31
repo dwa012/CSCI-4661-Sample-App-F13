e8dbe6097f7fa4d221676891305da5f71f43e6d0
Daniel Ward <dwa012@gmail.com>
2013-10-31 17:50:38 -0500

    a start to the solution

--- Android Studio/GroceryList/src/main/java/edu/uno/csci4661/grocerylist/database/GroceryProvider.java
+++ Android Studio/GroceryList/src/main/java/edu/uno/csci4661/grocerylist/database/GroceryProvider.java
@@ -2,8 +2,14 @@ package edu.uno.csci4661.grocerylist.database;
 
 import android.content.ContentProvider;
 import android.content.ContentValues;
+import android.content.Context;
+import android.content.UriMatcher;
 import android.database.Cursor;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+import android.database.sqlite.SQLiteQueryBuilder;
 import android.net.Uri;
+import android.text.TextUtils;
 
 /**
  * Created by danielward on 10/31/13.
@@ -12,19 +18,90 @@ public class GroceryProvider extends ContentProvider {
 
     public static final String AUTHORITY = "edu.uno.csci4661.grocerylist";
 
+    // the primary content URI
+    public static final Uri CONTENT_URI =
+            Uri.parse("content://" + AUTHORITY + "/items");
+
+    // database columns
+    public static final String KEY_ID = "_id";
+    public static final String KEY_NAME = "name";
+    public static final String KEY_DESCRIPTION = "description";
+    public static final String KEY_QUANTITY = "quantity";
+
+    //Create the constants used to differentiate between the different URI
+    //requests.
+    private static final int ITEMS = 1;
+    private static final int ITEM_ID = 2;
+
+    // the URI matcher
+    private static final UriMatcher uriMatcher;
+    static {
+        uriMatcher = new UriMatcher(UriMatcher.NO_MATCH);
+        uriMatcher.addURI("edu.uno.csci4661.grocerylist", "items", ITEMS);
+        uriMatcher.addURI("edu.uno.csci4661.grocerylist", "items/#", ITEM_ID);
+    }
+
+    private GroceryDatabaseHelper dbHelper;
+
     @Override
     public boolean onCreate() {
-        return false;
+        Context context = getContext();
+
+        dbHelper = new GroceryDatabaseHelper(context);
+
+        return true;
     }
 
     @Override
-    public Cursor query(Uri uri, String[] strings, String s, String[] strings2, String s2) {
-        return null;
+    public Cursor query(Uri uri,
+                        String[] projection,
+                        String selection,
+                        String[] selectionArgs,
+                        String sort) {
+
+        SQLiteDatabase database = dbHelper.getWritableDatabase();
+
+        SQLiteQueryBuilder qb = new SQLiteQueryBuilder();
+
+        qb.setTables(GroceryDatabaseHelper.ITEM_TABLE);
+
+        // If this is a row query, limit the result set to the passed in row.
+        switch (uriMatcher.match(uri)) {
+            case ITEM_ID: qb.appendWhere(KEY_ID + "=" + uri.getPathSegments().get(1));
+                break;
+            default      : break;
+        }
+
+        // If no sort order is specified, sort by date / time
+        String orderBy;
+        if (TextUtils.isEmpty(sort)) {
+            orderBy = KEY_ID;
+        } else {
+            orderBy = sort;
+        }
+
+        // Apply the query to the underlying database.
+        Cursor c = qb.query(database,
+                projection,
+                selection, selectionArgs,
+                null, null,
+                orderBy);
+
+        // Register the contexts ContentResolver to be notified if
+        // the cursor result set changes.
+        c.setNotificationUri(getContext().getContentResolver(), uri);
+
+        // Return a cursor to the query result.
+        return c;
     }
 
     @Override
     public String getType(Uri uri) {
-        return null;
+        switch (uriMatcher.match(uri)) {
+            case ITEMS: return "vnd.android.cursor.dir/edu.uno.csci4661.grocerylist";
+            case ITEM_ID: return "vnd.android.cursor.item/edu.uno.csci4661.grocerylist";
+            default: throw new IllegalArgumentException("Unsupported URI: " + uri);
+        }
     }
 
     @Override
@@ -42,5 +119,33 @@ public class GroceryProvider extends ContentProvider {
         return 0;
     }
 
-    // TODO add a database helper class
+    //Helper class for opening, creating, and managing database version control
+    private static class GroceryDatabaseHelper extends SQLiteOpenHelper {
+
+        private static final String DATABASE_NAME = "earthquakes.db";
+        private static final int DATABASE_VERSION = 1;
+        private static final String ITEM_TABLE = "items";
+
+        private static final String DATABASE_CREATE =
+                "create table " + ITEM_TABLE + " ("
+                        + KEY_ID + " integer primary key autoincrement, "
+                        + KEY_NAME + " TEXT, "
+                        + KEY_DESCRIPTION + " TEXT, "
+                        + KEY_QUANTITY + " INTEGER);";
+
+        public GroceryDatabaseHelper(Context context) {
+            super(context, DATABASE_NAME, null, DATABASE_VERSION);
+        }
+
+        @Override
+        public void onCreate(SQLiteDatabase db) {
+            db.execSQL(DATABASE_CREATE);
+        }
+
+        @Override
+        public void onUpgrade(SQLiteDatabase db, int oldVersion, int newVersion) {
+            db.execSQL("DROP TABLE IF EXISTS " + ITEM_TABLE);
+            onCreate(db);
+        }
+    }
 }
